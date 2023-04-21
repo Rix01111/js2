656968ed34b4767e072533c974ab7f2331165410
odrix <roso061217hdfsnda4@soycecytem.mx>
2023-04-21 12:16:27 -0500

    first comt

---
+++ 5/main.js
@@ -0,0 +1,49 @@
+const A=()=>{
+    Swal.fire('hola ')
+}
+
+const b=()=>{
+    Swal.fire(
+        'como',
+        'estas',
+        'bro'
+      )
+}
+
+
+const c=()=>{
+    Swal.fire({
+        icon: 'exeso de wapura',
+        title: 'Oops...',
+        text: 'ala',
+        footer: 'cada dia mas programador'
+      })
+}
+
+
+
+
+
+
+
+const d=()=>{
+    Swal.fire({
+        imageUrl: 'https://placeholder.pics/svg/300x1500',
+        imageHeight: 1500,
+        imageAlt: 'popo'
+      })
+}
+
+
+
+
+
+const e=()=>{
+    Swal.fire({
+        position: 'programador',
+        icon: 'danonino',
+        title: 'loloa',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
